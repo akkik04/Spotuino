ba81550354166142ebd2378e533757e3b59f461b
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-08-16 14:00:24 -0400

    Updated python script for song detection.
    This is a part of the software for the project written in Python. This part of the software receives the keyword signals sent from the Arduino/C++ script through the COM3 port, and uses the PyAutoGUI module to automate the functions onto the Spotify app (i.e. previous track, pause track, next track). I updated this script to include a module for song detection called SwSpotify. The script can now send the song name to the Arduino through the COM3 port for display through the 1602 LCD.

--- Arduino-Controlled-Spotify/SpotifyController.py
+++ Arduino-Controlled-Spotify/SpotifyController.py
@@ -5,14 +5,16 @@
 # 1. PyAutoGUI module is mandatory as we will use its functions for the automation process.
 # 2. Time module is mandatory because we can use its functions to create effects and suspend execution of a thread for 'x' seconds.
 # 3. Serial module is the whole base for this project. Since we need to communicate through the serial port, this module grants access for that purpose.
+# 4. SwSpotify module is necessary for the add-on of song-detection. The song is detected through this module's functions and is sent to the Arduino for display through a 1602 LCD.
 import pyautogui, time, serial
+from SwSpotify import spotify
 
 # creating a variable to open the port at 'COM3' for the communication process at 9600 baud.
 # the functions for 'serial.Serial' is: __init__(port, baudrate)
 arduino_uno = serial.Serial('com3', 9600)
 
-# code to use the time.sleep() function to suspend the execution for 1.75 seconds.
-time.sleep(1.75)
+# creating a variable to store the old song that was playing. This variable is crucial for song detection.
+old_song = " "
 
 # creating a while-loop.
 while True:
@@ -36,14 +38,35 @@ while True:
         # using the PyAutoGui module's keyboard function called 'typewrite' to activate the space bar to stop the music in a time interval of 0.2 seconds.
         pyautogui.typewrite(['space'], 0.2)
 
+        # time delay.
+        time.sleep(0.2)
+
     elif 'back' in signal:
 
         # code to play the previous song if the keyword 'back' is detected through the outputted signal.
         # using the PyAutoGui module's keyboard function called 'hotkey' to activate the 'ctrl + left' input to play the previous song.
         pyautogui.hotkey('ctrl', 'left')
 
+        # time delay.
+        time.sleep(0.2)
+
     elif 'next' in signal:  
 
         # code to play the next song if the keyword 'next' is detected through the outputted signal.
         # using the same PyAutoGui 'hotkey' function as before to activate the 'ctrl + right' input to play the upcoming/next song.
         pyautogui.hotkey('ctrl', 'right')
+
+        # time delay.
+        time.sleep(0.2)
+
+    # creating a variable to continuously update the current song that is playing. This variable is crucial for song detection.
+    current_song = spotify.song()
+
+    # creating a nested if-statement to send the arduino the song name if the previous song and the current song are different.
+    if current_song != old_song:
+
+        # using the pySerial's 'write()' function to write the song name with encoding to the arduino through the COM3 port.
+        arduino_uno.write(spotify.song().encode())
+
+    # code to tie/update the old song to the current song after the iteration.
+    song_old = current_song
